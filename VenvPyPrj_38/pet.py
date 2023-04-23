a5ca7d963e0695f7cc161861f9f69fcf0bef45f5
VladislavKornyushin <kornyushin.vladislav@yandex.ru>
2023-04-23 21:51:14 +0300

    Exercise 23/04/2023

---
+++ VenvPyPrj_38/pet.py
@@ -0,0 +1,16 @@
+# Задание 1.8.2
+# Создайте класс Dog с помощью наследования класса Cat. Создайте метод get_pet() таким образом, чтобы он возвращал только имя и возраст.
+# Далее сделайте вывод этой информации в консоль
+
+from cats import CatClass
+
+class Dog(CatClass):
+    def __init__(self, name=' - ', age=0):
+        self.name = name
+        self.age = age
+    def get_pet(self):
+        return f'{self.get_name()} {self.get_age()}'
+    # def get_name(self):
+    #     return self.name
+    # def get_age(self):
+    #     return self.age
