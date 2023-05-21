e5f828db1d659db50c5ee27530429cdcf3448232
VladislavKornyushin <kornyushin.vladislav@yandex.ru>
2023-05-21 23:22:58 +0300

    Exercise 21/05/2023

--- VenvPyPrj_38/myexchange.py
+++ VenvPyPrj_38/myexchange.py
@@ -1,40 +1,47 @@
-import redis  # импортируем библиотеку
+# Задание
+# Напишите Telegram-бота, в котором будет реализован следующий функционал:
+#  1. Бот возвращает цену на определённое количество валюты (евро, доллар или рубль).
+#  2. При написании бота необходимо использовать библиотеку pytelegrambotapi.
+#  3. Человек должен отправить сообщение боту в виде <имя валюты, цену которой он хочет узнать> <имя валюты, в которой
+#     надо узнать цену первой валюты> <количество первой валюты>.
+#  4. При вводе команды /start или /help пользователю выводятся инструкции по применению бота.
+#  5. При вводе команды /values должна выводиться информация о всех доступных валютах в читаемом виде.
+#  6. Для получения курса валют необходимо использовать любое удобное API и отправлять к нему запросы с помощью
+#     библиотеки Requests.
+#  7. Для парсинга полученных ответов использовать библиотеку JSON.
+#  8. При ошибке пользователя (например, введена неправильная или несуществующая валюта или неправильно введено число)
+#     вызывать собственно написанное исключение APIException с текстом пояснения ошибки.
+#  9. Текст любой ошибки с указанием типа ошибки должен отправляться пользователю в сообщения.
+# 10. Для отправки запросов к API описать класс со статическим методом get_price(), который принимает три аргумента и
+#     возвращает нужную сумму в валюте:
+#      - имя валюты, цену на которую надо узнать, — base;
+#      - имя валюты, цену в которой надо узнать, — quote;
+#      - количество переводимой валюты — amount.
+# 11. Токен Telegram-бота хранить в специальном конфиге (можно использовать .py файл).
+# 12. Все классы спрятать в файле extensions.py.
+
 import json
 import requests
 from bs4 import BeautifulSoup as bs
-# from time import time
-# from pprint import pprint
 
-currency_dict = {'RUR': ['российский рубль', 'рубль', 'рубли'],
-                 'USD': ['доллары сша', 'доллар', 'доллары', 'баксы', 'баксов', 'us dollar', 'US Dollar'],
-                 'EUR': ['евро'],
-                 'CNY': ['китайский юань', 'юань', 'доллары', 'баксы', 'баксов'],
+currency_dict = {'RUB': ['Российский рубль', 'рубль', 'рублей', 'Russian Ruble'],
+                 'USD': ['Доллары сша', 'доллар', 'долларов', 'баксы', 'баксов', 'us dollar', 'US Dollar'],
+                 'EUR': ['Евро', 'евро', 'евро', 'Euro'],
+                 'CNY': ['Китайский юань', 'юань', 'юаней', 'Chinese Yuan Renminbi'],
                  }
 
-def get_exchange_list_xrates(currency, amount=1):
-    # создаем запрос к x-rates.com для получения валют и курсов
-    content = requests.get(f'https://www.x-rates.com/table/?from={currency}&amount={amount}').content
-    # инициализируем beautifulsoup
-    soup = bs(content, 'html.parser')
-    # получаем дату-время последнего обновления курса
-    # price_datetime = parse(soup.find_all("span", attrs={"class": "ratesTimestamp"})[1].text)
-    price_datetime = soup.find_all("span", attrs={"class": "ratesTimestamp"})[1].text
-    # получаем таблицу курсов
-    exchange_tables = soup.find_all('table')
-    exchange_rates = {}
-    for exchange_table in exchange_tables:
-        for tr in exchange_table.find_all('tr'):
-            tds = tr.find_all('td')
-            if tds:
-                currency = tds[0].text
-                # получим курс
-                exchange_rate = float(tds[1].text)
-                exchange_rates[currency] = exchange_rate
-    return price_datetime, exchange_rates
+def test_json(base, quote, amount=1):
+    # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    req_get = requests.get(f'https://www.x-rates.com/table/?from={base}&amount={amount}')
+    print(req_get.text)
+    js = json.loads(req_get.text)
+    print(js)
 
-def get_exchange_convert_sum(currency, amount=1):
+def get_exchange_convert_sum(base, quote, amount=1):
     # создаем запрос к x-rates.com для получения валют и курсов
-    content = requests.get(f'https://www.x-rates.com/table/?from=RUB&amount={amount}').content
+    # content = requests.get(f'https://www.x-rates.com/graph/?from={base}&to={quote}&amount={amount}').content
+    content = requests.get(f'https://www.x-rates.com/table/?from={base}&amount={amount}').content
+
     # инициализируем beautifulsoup
     soup = bs(content, 'html.parser')
     # получаем дату-время последнего обновления курса
@@ -46,83 +53,51 @@ def get_exchange_convert_sum(currency, amount=1):
         for tr in exchange_table.find_all('tr'):
             tds = tr.find_all('td')
             if tds:
-                for cur in range(len(currency_dict[currency])):
-                    if tds[0].text == currency_dict[currency][cur]:
-                        exchange_rate1 = round(float(tds[1].text), 2)  # курс продажи
-                        exchange_rate2 = round(float(tds[2].text), 2)  # курс покупки
-                        exchange_rates[currency] = exchange_rate2
+                for cur in range(len(currency_dict[quote])):
+                    if tds[0].text == currency_dict[quote][cur]:
+                        base_rp = currency_dict[base][2]
+                        quote_rp = currency_dict[quote][2]
+                        print(base_rp, ' --- ', quote_rp)
+                        exchange_rate1 = round(float(tds[1].text), 2)  # base/quote
+                        exchange_rate2 = round(float(tds[2].text), 2)  # quote/base
+                        exchange_rates[quote] = exchange_rate1
                         # print('tds[0]: ', tds[0])  # <td>US Dollar</td>
                         # print('tds[1]: ', tds[1])  # <td class="rtRates"><a href="https://www.x-rates.com/graph/?from=RUB&amp;to=USD">12.418301</a></td>
                         # print('tds[2]: ', tds[2])  # <td class="rtRates"><a href="https://www.x-rates.com/graph/?from=USD&amp;to=RUB">80.526314</a></td>
 
-                        return price_datetime, exchange_rates
+                        return price_datetime, exchange_rates, [base_rp, quote_rp]
 
     # return price_datetime, exchange_rates # если показывать все валюты
-def start_processing(input_list):
-    amount = int(input_list[-1])
-    input_list.pop()
-    name_currency = ' '.join(input_list)
+def get_price(input_list):
+    print(input_list)
+    base = input_list[0]
+    quote = input_list[1]
+    amount = round(float(input_list[-1]), 2)
+
+    base_code = ''
+    quote_code = ''
 
     for dict_key, dict_values in currency_dict.items():
         for i in range(len(dict_values)):
-            if dict_values[i] == name_currency.lower():
-                source_currency = dict_key
+            if dict_values[i] == base.lower():
+                base_code = dict_key
                 break
 
-    price_datetime, exchange_rates = get_exchange_convert_sum(source_currency, amount)
+    for dict_key, dict_values in currency_dict.items():
+        for i in range(len(dict_values)):
+            if dict_values[i] == quote.lower():
+                quote_code = dict_key
+                break
+
+    # a1, a2 = test_json(base_code, quote_code, amount)
+    # print(a1, ' ', a2)
+
+    # exchange_rates = get_exchange_convert_sum(base_code, quote_code, amount)
+    price_datetime, exchange_rates, curr_rp_list = get_exchange_convert_sum(base_code, quote_code, amount)
     print('Last updated:', price_datetime)
-    print(source_currency, ' ', exchange_rates[source_currency])
-
-def my_redis():
-    red = redis.Redis(
-        host='localhost',
-        port=6379,
-        password=''
-    )
-
-    # red.delete('dict1')  # удаляются ключи с помощью метода .delete()
-    # print(red.get('dict1'))
-    #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    # dict1 = {'key1': 'value1', 'key2': 'value2'}  # создаём словарь для записи
-    # red.set('dict1', json.dumps(dict1))  # с помощью функции dumps() из модуля json превратим наш словарь в строчку
-    # converted_dict = json.loads(
-    #     red.get('dict1'))  # с помощью знакомой нам функции превращаем данные, полученные из кеша обратно в словарь
-    # print(type(converted_dict))  # убеждаемся, что мы получили действительно словарь
-    # print(converted_dict)  # ну и выводим его содержание
-    #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    # red = redis.Redis(
-    #     host='localhost',
-    #     port=6379,
-    #     password=''
-    # )
-    #
-    # print(red.get('var1'))  # считываем из кеша данные
-    #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    # red = redis.Redis(
-    #     host='localhost',
-    #     # ваш хост, если вы поставили Редис к себе на локальную машину, то у вас это будет localhost. Если же вы находитесь на Windows, то воспользуйтесь полем host из вашей облачной БД, которую мы создавали в скринкасте.
-    #     port=6379,
-    #     # порт подключения. На локальной машине это должно быть 6379. Для пользователей облачного сервиса порт всегда разный, поэтому его надо копировать оттуда же, что и host.
-    #     password=''
-    #     # для локальной машины пароль не требуется (если вы устанавливали Редис к себе на компьютер и не пользовались облачным сервисом из скринкаста выше). Для пользователей облачного сервиса пароль находится в вашей облачной базе данных в поле password
-    # )
-    # red.set('var1', 'value1')  # записываем в кеш строку "value1"
-    # print(red.get('var1'))  # считываем из кеша данные
-
+    print(base_code, ' ', exchange_rates)
+    return price_datetime, exchange_rates, curr_rp_list
 
 if __name__ == '__main__':
-    my_redis()
-    # start_processing(['Доллары', 'США', '1000'])
-    # import sys
-    # print(sys.argv)
-    # source_currency = sys.argv[1]
-    # amount = float(sys.argv[2])
-    # target_currency = 'RUB'  #"GBP"
-    # price_datetime, exchange_rates = get_exchange_list_xrates(source_currency, amount)
-    # print('Last updated:', price_datetime)
-    # pprint(exchange_rates)
+    get_price(['евро', 'доллары', '1'])
 
-# [<td>Chinese Yuan Renminbi</td>,
-# <td class="rtRates">
-# <a href="https://www.x-rates.com/graph/?from=USD&amp;to=CNY">7013.490458</a></td>,
-# <td class="rtRates"><a href="https://www.x-rates.com/graph/?from=CNY&amp;to=USD">0.142582</a></td>]
